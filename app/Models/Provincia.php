29e7bb1a8c860a8719f083747e5d7f0fd58e0312
Analuz Dahlquist <you@example.com>
2025-05-18 22:46:07 -0300

    Modifica la tabla creada de provincia

--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Provincia extends Model
 {
-    //
+    use SoftDeletes;
+    
+    /**
+     * Indicates if the models ID is auto-incrementing. 
+     * @var bool
+    */
+    public $incrementing = true;
+
+    /**
+     * The primaty key associated with the table.
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The table associated with the model.
+     * @var string
+     */
+    protected $table = 'provincias';
+
+    
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected$fillable = [
+        'nombre',
+        'codigo'
+    ];
 }
