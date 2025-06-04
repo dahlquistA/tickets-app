3f62947d016e6716a8e8203d84c769d275c22a9d
Analuz Dahlquist <you@example.com>
2025-06-04 18:47:52 -0300

    Agrega nuevo archivo Seeder de Provincia

---
+++ database/seeders/ProvinciaSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Ciudad;
+use App\Models\Provincia;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+
+class ProvinciaSeeder extends Seeder {
+
+    public function run(): void
+    {
+        $response = Http::get('https://apis.datos.gob.ar/georef/api/provincias');
+        collect($response->object()->provincias)->map(function (object $provincia){
+            $provincia = Provincia::updateOrCreate([
+                'codigo'=> $provincia->id,
+            ], [
+                'nombre' => $provincia->nombre
+            ]);
+        });
+
+    }
+}
